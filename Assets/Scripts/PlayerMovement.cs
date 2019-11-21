90f73edad17b10a1cc7e17a41c993e56308c7648
Marc Sevilleja <marc@sevilayha.com>
2019-11-20 23:19:48 -0800

    Added animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;   // stores x (horiz) and y (vert)
+
+    void Update() {
+        // Inputs
+        movement.x = Input.GetAxisRaw("Horizontal");     // value btwn -1 and 1
+        movement.y = Input.GetAxisRaw("Vertical");       // works default with WASD and arrow keys
+
+    }
+
+    // Movement
+    void FixedUpdate() {
+        // Movement
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
