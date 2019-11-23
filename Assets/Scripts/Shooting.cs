4ebaa028cbbdc897480e484c4b58d569519c59ba
Marc Sevilleja <marc@sevilayha.com>
2019-11-22 21:45:31 -0800

    Created Script for shooting animation check

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,14 +6,36 @@ public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject arrowPrefab;
+    public Animator playerAnim;
 
     public float arrowForce = 5f;
+    private float horizontal;
+    private float vertical;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
+        {
+            horizontal = playerAnim.GetFloat("Facing Horizontal");
+            vertical = playerAnim.GetFloat("Facing Vertical");
+
+            if (vertical == -1)
+                playerAnim.SetFloat("Vertical Shoot", -1f);          // if aiming down, shoot down
+            else if (vertical == 1)
+                playerAnim.SetFloat("Vertical Shoot", 1f);         // if aiming up, shoot up
+            else
+                playerAnim.SetFloat("Vertical Shoot", 0f);
+
+            if (vertical == 0 && horizontal == -1)
+                playerAnim.SetFloat("Horizontal Shoot", -1f);        // if aiming left, shoot left
+            else if (vertical == 0 && horizontal == 1)
+                playerAnim.SetFloat("Horizontal Shoot", 1f);       // if aiming right, shoot right
+            else
+                playerAnim.SetFloat("Horizontal Shoot", 0f);
+
             Shoot();
+        }
     }
 
     void Shoot()
