80b57d4d773bc4ce4ae0598d8c82f3ef0f77b937
Marc Sevilleja <marc@sevilayha.com>
2020-01-03 02:40:03 -0800

    Firepoints, animation, projectile pass

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float baseMoveSpeed;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject projectile;
+    [SerializeField] List<Transform> firePoints;
+    [SerializeField] float projectileSpeed = 5f;
+    [SerializeField] float projectileFreq = 0.25f;
+    [SerializeField] float baseMoveSpeed;
+
+
+    Coroutine firingCoroutine;
+    private bool alive = true;
     private float updatedMoveSpeed;
 
-    public Camera myCamera;
-    public Rigidbody2D rb;
-    public Animator anim;
-
     Vector2 movement;   // stores x (horiz) and y (vert)
-    Vector2 mousePos;
 
     private void Start()
     {
@@ -21,17 +26,21 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        Move();
+        ReadyFire();
+    }
+
+    private void Move()
+    {
         // Movement inputs
         movement.x = Input.GetAxisRaw("Horizontal");        // value btwn -1 and 1
         movement.y = Input.GetAxisRaw("Vertical");          // works default with WASD and arrow keys
 
         if (Input.GetKeyDown(KeyCode.LeftShift))            // press and hold shift to move faster
-            updatedMoveSpeed = baseMoveSpeed * 0f;
+            updatedMoveSpeed = 0f;
         else if (Input.GetKeyUp(KeyCode.LeftShift))
             updatedMoveSpeed = baseMoveSpeed;
 
-        //anim.SetFloat("Horizontal", movement.x);
-        //anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Base Speed", updatedMoveSpeed);
         anim.SetFloat("Speed", movement.sqrMagnitude);      // sqrMag will always be pos, optimal since sqr root is unneeded
 
@@ -56,7 +65,37 @@ public class PlayerMovement : MonoBehaviour
             anim.SetFloat("Vertical", -1);
             anim.SetFloat("Horizontal", 0);
         }
+    }
 
+    private void ReadyFire()
+    {
+        if (alive)
+        {
+            if (Input.GetButtonDown("Fire1"))
+                firingCoroutine = StartCoroutine(Fire());
+
+            if (Input.GetButtonUp("Fire1"))
+                StopCoroutine(firingCoroutine);
+        }
+        else
+            StopCoroutine(firingCoroutine);
+    }
+
+    IEnumerator Fire()
+    {
+        anim.SetBool("Shoot", true);
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length + anim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        anim.SetBool("Shoot", false);
+    }
+
+
+    public void ProjectileFly(int facing)
+    {
+        GameObject arrow = Instantiate(
+            projectile,
+            firePoints[facing].position,
+            Quaternion.identity) as GameObject;
+        arrow.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
     }
 
     // Movement
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject projectile;
+    [SerializeField] List<Transform> firePoints;
+    [SerializeField] float projectileSpeed = 5f;
+    [SerializeField] float projectileFreq = 0.25f;
+
+    Coroutine firingCoroutine;
+    bool alive = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FireContinuously()
+    {
+        anim.SetBool("Shooting", true);
+        //yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length + anim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        while (true)
+        {
+            GameObject laser = Instantiate(
+                    projectile,
+                    transform.position,
+                    Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+
+            yield return new WaitForSeconds(projectileFreq);
+            anim.SetBool("Shooting", false);
+        }
+        
+    }
+
+    public void Fire()
+    {
+        if (alive)
+        {
+            if (Input.GetButtonDown("Fire1"))
+                firingCoroutine = StartCoroutine(FireContinuously());
+
+            if (Input.GetButtonUp("Fire1"))
+                StopCoroutine(firingCoroutine);
+        }
+        else
+            StopCoroutine(firingCoroutine);
+    }
+}
