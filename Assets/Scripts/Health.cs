fa04f59d92ba2c4ce3c2797593db7ea820776874
Marc Sevilleja <marc@sevilayha.com>
2020-01-03 22:54:00 -0800

    Health and damage script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 200f;
+    [SerializeField] float deathWait = 1f;
+    Animator anim;
+    Collider2D m_Collider;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        m_Collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    public void Damage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+            StartCoroutine(Death());
+        
+    }
+
+    IEnumerator Death()
+    {
+        anim.SetBool("Death", true);
+        m_Collider.enabled = !m_Collider.enabled;
+        yield return new WaitForSeconds(deathWait);
+        //Destroy(gameObject);
+    }
+
+}
