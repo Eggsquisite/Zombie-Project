f20aea341071674c3aa2a130542fe9428807ee88
Marc Sevilleja <marc@sevilayha.com>
2020-01-09 23:45:01 -0800

    Misc tweaks

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] List<GameObject> enemyPrefab;
+    [SerializeField] float spawnRate = 5f;
+    private float spawnTimer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        spawnTimer += Time.time;
+        if (spawnTimer >= spawnRate)
+        {
+            Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Count)], transform.position, Quaternion.identity);
+            spawnTimer = 0;
+            spawnRate += Time.time;
+            Debug.Log("here" + Time.time);
+        }
     }
 }
